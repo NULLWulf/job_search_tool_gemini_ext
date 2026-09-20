ce369ff07fc7a5044485c15f71c365e134e0ac48
Nathan Wolf <ndwolf1991@gmail.com>
2026-09-20 06:07:03 -0400

    added QOL locals.sh script for copying config files

---
+++ locals.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# copies the examples to working files for the user
+
+cp aggregators.example.yaml aggregators.yaml
+cp companies.example.yaml companies.yaml
+cp filters.example.yaml filters.yaml
+cp profile.example.yaml profile.yaml
